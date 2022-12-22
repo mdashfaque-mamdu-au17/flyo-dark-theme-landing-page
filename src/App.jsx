7411f3eab14c81eaf9b2266458eb83733169237c
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-12-22 12:23:03 +0530

    initial configuration

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="text-green-300">Hello world</div>;
+}
+
+export default App;
