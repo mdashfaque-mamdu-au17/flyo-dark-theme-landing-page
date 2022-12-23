7ce95b32f7591f8e7b1adf014c73b311fe629076
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-12-23 22:45:41 +0530

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Button from './components/Button/Button';
 import Link from './components/Button/Link';
 
 function App() {
@@ -6,6 +7,9 @@ function App() {
       <Link type="navigation">Sign in</Link>
       <div className="mt-10">
         <Link type="navigation-footer">About</Link>
+      </div>
+      <div className="mt-10 w-[500px] mx-auto">
+        <Button btnType="primary">Get Started For Free</Button>
       </div>
     </div>
   );
