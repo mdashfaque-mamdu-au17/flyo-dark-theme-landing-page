1b4db654339d3bd36f2cbf29fe8795af181c82aa
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-12-27 13:13:23 +0530

    color changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,14 +5,19 @@ module.exports = {
     extend: {
       colors: {
         'primary-black': '#181F2B',
+        'primary-black-600': '#07043B',
         'secondary-black': '#202A3C',
+        'light-dark': '#1B2330',
         'creame-white': '#FFFFFF',
         'primary-cyan': '#63E1D9',
         'dark-cyan': '#8ADAE3',
+        'light-gray': '#C0C0C0',
+        'primary-red': '#FF4242',
       },
       boxShadow: {
         '3xl': '2px 3px 6px rgba(0, 0, 0, 0.15925);',
         '4xl': '5px 5px 8px 4px rgba(56, 56, 56, 0.0991282)',
+        'input-shadow': '1px 1px 3px rgba(7, 4, 59, 0.157043)',
       },
     },
   },
