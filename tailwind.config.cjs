c883f62754d0ac3cc950735739e584ec9c13c293
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-12-27 11:13:20 +0530

    box shadow added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,12 +5,14 @@ module.exports = {
     extend: {
       colors: {
         'primary-black': '#181F2B',
+        'secondary-black': '#202A3C',
         'creame-white': '#FFFFFF',
         'primary-cyan': '#63E1D9',
         'dark-cyan': '#8ADAE3',
       },
       boxShadow: {
         '3xl': '2px 3px 6px rgba(0, 0, 0, 0.15925);',
+        '4xl': '5px 5px 8px 4px rgba(56, 56, 56, 0.0991282)',
       },
     },
   },
