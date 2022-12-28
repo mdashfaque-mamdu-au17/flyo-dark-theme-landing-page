30a8a3fe4d87c294a87331d3efb733d84d97f839
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-12-28 17:07:54 +0530

    small correction

--- src/components/Sections/FormBox.jsx
+++ src/components/Sections/FormBox.jsx
@@ -5,13 +5,13 @@ import Form from './Form';
 const FormBox = () => {
   return (
     <section className="bg-light-dark pt-10 pb-[37px] rounded-[9px] px-[28px] max-w-[863px] mx-auto md:pt-[42px] md:pb-[41px]">
-      <div className="pb-6 md:pb-4">
+      <div className="pb-6 md:pb-4 text-center">
         <Heading type="secondary" styles="md:text-[32px] md:leading-[48px]">
           Get early access today
         </Heading>
       </div>
 
-      <div className="pb-6 md:px-[95px] md:pb-[38px]">
+      <div className="pb-6 md:px-[95px] md:pb-[38px] text-center">
         <Paragraph type="secondary">
           It only takes a minute to sign up and our free starter tier is
           extremely generous. If you have any questions, our support team would
