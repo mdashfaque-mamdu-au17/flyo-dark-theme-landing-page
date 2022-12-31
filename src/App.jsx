3ff0d0c2d42dd76275e5f747694ae420518b0bfd
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-12-31 11:48:57 +0530

    commented testing compnents

--- src/App.jsx
+++ src/App.jsx
@@ -14,31 +14,31 @@ function App() {
     <div className="">
       <Navbar />
       <Hero />
-      <Link type="navigation">Sign in</Link>
+      {/* <Link type="navigation">Sign in</Link>
       <div className="mt-10">
         <Link type="navigation-footer">About</Link>
       </div>
       <div className="mt-10  mx-auto">
         <Button btnType="primary">Get Started For Free</Button>
-      </div>
+      </div> */}
 
-      <div className="mt-10">
+      {/* <div className="mt-10">
         <Heading type="primary">
           All your files in one secure location, accessible anywhere.
         </Heading>
       </div>
       <div className="mt-10">
         <Heading type="secondary">Stay productive, wherever you are</Heading>
-      </div>
+      </div> */}
 
       {/* on contact form */}
-      <div className="mt-10">
+      {/* <div className="mt-10">
         <Heading type="secondary" styles="md:text-[32px] md:leading-[48px]">
           Get early access today
         </Heading>
-      </div>
+      </div> */}
 
-      <div className="mt-10">
+      {/* <div className="mt-10">
         <Heading type="other-type">Access your files, anywhere</Heading>
       </div>
 
@@ -48,9 +48,9 @@ function App() {
           Access them wherever you need, share and collaborate with friends
           family, and co-workers.
         </Paragraph>
-      </div>
+      </div> */}
 
-      <div className="mt-10">
+      {/* <div className="mt-10">
         <Paragraph type="secondary">
           The ability to use a smartphone, tablet, or computer to access your
           account means your files follow you everywhere.
@@ -62,9 +62,9 @@ function App() {
           Never let location be an issue when accessing your files. Fylo has you
           covered for all of your file storage needs.
         </Paragraph>
-      </div>
+      </div> */}
 
-      <div className="mt-10">
+      {/* <div className="mt-10">
         <Paragraph type="fifth">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua
@@ -81,7 +81,7 @@ function App() {
 
       <div className="mt-10 mx-5 lg:mx-0">
         <FormBox />
-      </div>
+      </div> */}
     </div>
   );
 }
