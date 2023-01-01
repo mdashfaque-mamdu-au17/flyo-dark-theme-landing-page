0a80fab507a3b39c300600b5fadeaf7597186534
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-01-01 11:45:03 +0530

    passed props from outside

--- src/components/Card/FeatureCard.jsx
+++ src/components/Card/FeatureCard.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Heading from '../Texts/Heading';
 import Paragraph from '../Texts/Paragraph';
-import AnyWhereIcon from '../../assets/icon-access-anywhere.svg';
 
 const FeatureCard = (props) => {
   const { icon, title, description, width } = props;
@@ -11,18 +10,15 @@ const FeatureCard = (props) => {
     <div className="flex flex-col justify-center">
       <div className="flex justify-center">
         <div className="h-[104px] w-[104px] flex justify-center items-center">
-          <img src={AnyWhereIcon} alt="" className={classNames(width)} />
+          <img src={icon} alt="" className={classNames(width)} />
         </div>
       </div>
 
       <div className="pt-6 md:pt-4">
-        <Heading type="other-type">Access your files, anywhere</Heading>
+        <Heading type="other-type">{title}</Heading>
       </div>
       <div className="pt-2">
-        <Paragraph type="secondary">
-          The ability to use a smartphone, tablet, or computer to access your
-          account means your files follow you everywhere.
-        </Paragraph>
+        <Paragraph type="secondary">{description}</Paragraph>
       </div>
     </div>
   );
