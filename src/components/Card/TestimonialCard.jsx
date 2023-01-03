b70fa33358c028b46e6ad1aafb1516cb94894cda
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-01-03 12:34:25 +0530

    passed props from outside

--- src/components/Card/TestimonialCard.jsx
+++ src/components/Card/TestimonialCard.jsx
@@ -1,29 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Paragraph from '../Texts/Paragraph';
-import Satish from '../../assets/profile-1.jpg';
+
 const TestimonialCard = (props) => {
   const { review, photo, name, profession } = props;
   return (
-    <div className="bg-secondary-black rounded pt-6 pb-[22px] px-[20px] max-w-[280px] shadow-4xl md:max-w-[360px] md:pt-10 md:pb-[26px] md:pl-[26px] md:pr-6">
+    <div className="relative z-20 bg-secondary-black rounded pt-6 pb-[22px] px-[20px] max-w-[280px] shadow-4xl md:max-w-[360px] md:pt-10 md:pb-[26px] md:pl-[26px] md:pr-6">
       <div className="text-left pb-[17px] md:pb-6">
-        <Paragraph type="fourth">
-          Fylo has improved our team productivity by an order of magnitude.
-          Since making the switch our team has become a well-oiled collaboration
-          machine.
-        </Paragraph>
+        <Paragraph type="fourth">{review}</Paragraph>
       </div>
 
       <div className="flex gap-2 items-center">
         <div>
-          <img src={Satish} alt="" className="h-6 w-6 rounded-full" />
+          <img src={photo} alt="" className="h-6 w-6 rounded-full" />
         </div>
         <div className="flex flex-col justify-start gap-1">
           <p className="text-[10px] text-left font-bold leading-3 text-creame-white">
-            Satish Patel
+            {name}
           </p>
           <p className="text-[7px] text-creame-white leading-[10px] tracking-[0.5px]">
-            Founder & CEO, Huddle
+            {profession}
           </p>
         </div>
       </div>
